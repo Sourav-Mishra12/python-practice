4e2d128765a7cb30314b8e2c2f37f9c0f73ac5f3
sourav mishra <cag.souravmishra@gmail.com>
2025-10-21 23:16:20 +0530

    figuring out why we need stats

--- numpy/somewhat_basics.py
+++ numpy/somewhat_basics.py
@@ -12,9 +12,37 @@ import numpy as np
 
 
 
-temp = [28, 30, 31, 33, 29, 30, 31]
-print(f"My goodness the average temperature was :{np.ceil(np.mean(temp))} , DAMMNN")
-print(f"The MIDDLE east of temps is here : {np.median(temp)} , NICEE")
-values , counts = np.unique(temp , return_counts=True)
-mode_temp = values[counts == np.max(counts)]
-print(f"Here comes the repetitor : {mode_temp} , don't repeat be unique ")
+#temp = [28, 30, 31, 33, 29, 30, 31]
+
+#print(f"My goodness the average temperature was :{np.ceil(np.mean(temp))} , DAMMNN")
+#print(f"The MIDDLE east of temps is here : {np.median(temp)} , NICEE")
+#values , counts = np.unique(temp , return_counts=True)
+#mode_temp = values[counts == np.max(counts)]
+#print(f"Here comes the repetitor : {mode_temp} , don't repeat be unique ")
+
+
+#factory_productions = [29,78,88,99,120,31,56,87]
+
+#mean = np.mean(factory_productions)
+#median= np.median(factory_productions)
+#values,counts = np.unique(factory_productions , return_counts = True)
+#mode = values[counts == np.max(counts)]
+
+#print("MEAN" ,mean)
+#print("MEDIAN" , median)
+# print("MODE" , mode)
+
+
+shoe_sizes = [8, 8, 9, 9, 9, 10, 10, 11, 8, 9]
+
+mean =  np.mean(shoe_sizes)
+median = np.median(shoe_sizes)
+values , counts = np.unique(shoe_sizes , return_counts= True)
+mode =  values[counts == np.max(counts)]
+
+print("mean:",mean)
+print("median:",median)
+print("mode:",mode)
+print("THE MODE IS THE BEST FOR THE COMPANY FOR ANALYSIS")
+
+
